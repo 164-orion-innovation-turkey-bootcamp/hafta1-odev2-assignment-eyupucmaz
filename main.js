e263477a6ece903975b846457232257d27631b6b
Eyup Ucmaz <eyupucmaz@gmail.com>
2022-02-22 16:17:06 +0300

    :hammer: Added combination of winning and gameState object

--- main.js
+++ main.js
@@ -17,3 +17,17 @@ let player2 = "O";
 let currentPlayer = player1;
 let gameOver = false;
 let winner = "";
+
+// Winning Combinations
+let winCombos = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6],
+];
+// Initilaze game state object
+let gameState = {};
