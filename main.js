e43dcb4ecdf721f999d7e225e5fe3f19d814ab71
Eyup Ucmaz <eyupucmaz@gmail.com>
2022-02-24 14:41:00 +0300

    :art: add check winner func and isDraw and fireworks
    Checking if is there any winner or is game state draw and depend on state showing a result

--- main.js
+++ main.js
@@ -55,6 +55,37 @@ function changeCurrentPlayer() {
 		? (currentPlayer = player2)
 		: (currentPlayer = player1);
 }
+function fireworks() {
+	var duration = 3 * 1000;
+	var animationEnd = Date.now() + duration;
+	var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+	function randomInRange(min, max) {
+		return Math.random() * (max - min) + min;
+	}
+	var interval = setInterval(function () {
+		var timeLeft = animationEnd - Date.now();
+
+		if (timeLeft <= 0) {
+			return clearInterval(interval);
+		}
+
+		var particleCount = 50 * (timeLeft / duration);
+		// since particles fall down, start a bit higher than random
+		confetti(
+			Object.assign({}, defaults, {
+				particleCount,
+				origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+			})
+		);
+		confetti(
+			Object.assign({}, defaults, {
+				particleCount,
+				origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+			})
+		);
+	}, 250);
+}
 
 // Setting box states with data-num attribute
 function setGameState() {
@@ -68,6 +99,55 @@ function setGameState() {
 	});
 }
 
+// Checking gameState to is there any winner
+function checkWinner() {
+	winCombos.forEach((combo, index) => {
+		if (gameState[combo[0]] && gameState[combo[1]] && gameState[combo[2]]) {
+			if (
+				gameState[combo[0]] === gameState[combo[1]] &&
+				gameState[combo[1]] === gameState[combo[2]] &&
+				gameState[combo[0]] === gameState[combo[2]]
+			) {
+				console.log(
+					gameState[combo[0]],
+					gameState[combo[1]],
+					gameState[combo[2]]
+				);
+				gameOver = true;
+				winner = gameState[combo[0]];
+				player.textContent = `Player ${winner} is won!`;
+				resBtn.style.display = "block";
+				resBtn.addEventListener("click", restart);
+				fireworks();
+			}
+		}
+	});
+}
+// Checking if is game state is Draw
+function isDraw() {
+	if (
+		Object.values(gameState).every((value) => value != false) &&
+		gameOver === false
+	) {
+		winner = "Draw";
+		player.textContent = `It's ${winner}!`;
+		resBtn.style.display = "block";
+		resBtn.addEventListener("click", restart);
+	}
+}
+
+// Restarting the game, clearing all values
+function restart() {
+	gameState = {};
+	gameOver = false;
+	winner = "";
+	document.querySelectorAll(".box").forEach((box, index) => {
+		box.textContent = "";
+	});
+	player.textContent = `Current Player: ${currentPlayer}`;
+	resBtn.style.display = "none";
+}
+
 function handleBoxClick() {
 	let boxes = document.querySelectorAll(".box");
 	boxes.forEach((box, index) => {
@@ -77,16 +157,20 @@ function handleBoxClick() {
 					if (!gameState[index]) {
 						box.textContent = player1;
 						changeCurrentPlayer();
-						setGameState();
 						player.textContent = `Current Player: ${currentPlayer}`;
+						setGameState();
+						checkWinner();
+						isDraw();
 						console.log(gameState);
 					}
 				} else if (currentPlayer === player2) {
 					if (!gameState[index]) {
 						box.textContent = player2;
 						changeCurrentPlayer();
-						setGameState();
 						player.textContent = `Current Player: ${currentPlayer}`;
+						setGameState();
+						checkWinner();
+						isDraw();
 						console.log(gameState);
 					}
 				}
